576fa9b9eda3441f0ddbabd19f052716d15dfe23
Jeremy Gay <jeremy.gay22@gmail.com>
2023-08-24 17:40:29 +0100

    ci(build): fix with esbuild

---
+++ deployments/build.js
@@ -0,0 +1,21 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+  entryPoints: ['src/main.ts'],
+  bundle: true,
+  outfile: 'dist/main.js',
+  format: 'esm',
+  target: 'esnext',
+  platform: 'node',
+  banner: {
+    js: `
+import { fileURLToPath } from 'url';
+import { createRequire as topLevelCreateRequire } from 'module';
+import path from 'path';
+const require = topLevelCreateRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+        `,
+  },
+  external: ['@fastify/swagger', '@fastify/swagger-ui'],
+});
