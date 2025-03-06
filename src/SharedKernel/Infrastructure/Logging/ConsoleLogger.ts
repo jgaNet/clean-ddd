0a83efd5bfea894bfb257229e2807a0026ab25c5
Jeremy Gay <jeremy.gay22@gmail.com>
2025-03-06 10:46:54 +0100

    feat: better logs formatting

--- src/SharedKernel/Infrastructure/Logging/ConsoleLogger.ts
+++ src/SharedKernel/Infrastructure/Logging/ConsoleLogger.ts
@@ -11,7 +11,10 @@ export class ConsoleLogger implements Logger {
    */
   info(message: string, meta?: Record<string, unknown>): void {
     // eslint-disable-next-line no-console
-    console.info(`[INFO]${meta?.traceId ? `[${meta?.traceId}] ` : ''} ${message}`, meta && !meta.traceId ? meta : '');
+    console.info(
+      `${meta?.traceId ? `[${meta?.traceId}]` : '[************************************]'} [INFO]  ${message}`,
+      meta && !meta.traceId ? meta : '',
+    );
   }
 
   /**
@@ -21,7 +24,7 @@ export class ConsoleLogger implements Logger {
    */
   warn(message: string, meta?: Record<string, unknown>): void {
     // eslint-disable-next-line no-console
-    console.warn(`[WARN]${meta?.traceId ? `[${meta?.traceId}] ` : ''} ${message}`, meta && !meta.traceId ? meta : '');
+    console.warn(`${meta?.traceId ? `[${meta?.traceId}] ` : ''}[WARN] ${message}`, meta && !meta.traceId ? meta : '');
   }
 
   /**
@@ -33,7 +36,7 @@ export class ConsoleLogger implements Logger {
   error(message: string, error?: unknown, meta?: Record<string, unknown>): void {
     // eslint-disable-next-line no-console
     console.error(
-      `[ERROR]${meta?.traceId ? `[${meta?.traceId}] ` : ''} ${message}`,
+      `${meta?.traceId ? `[${meta?.traceId}] ` : ''}[ERROR] ${message}`,
       error || '',
       meta && !meta.traceId ? meta : '',
     );
@@ -46,6 +49,6 @@ export class ConsoleLogger implements Logger {
    */
   debug(message: string, meta?: Record<string, unknown>): void {
     // eslint-disable-next-line no-console
-    console.debug(`[DEBUG]${meta?.traceId ? `[${meta?.traceId}] ` : ''} ${message}`, meta && !meta.traceId ? meta : '');
+    console.debug(`${meta?.traceId ? `[${meta?.traceId}] ` : ''}[DEBUG] ${message}`, meta && !meta.traceId ? meta : '');
   }
 }
