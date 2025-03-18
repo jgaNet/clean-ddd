691117e769ea7f7f35a9e01cfc963c3d71e27f1b
Jeremy Gay <jeremy.gay22@gmail.com>
2025-03-18 18:53:07 +0100

    test: auth e2e tests

--- src/Contexts/Security/Presentation/API/REST/Routes/auth.routes.e2e.spec.ts
+++ src/Contexts/Security/Presentation/API/REST/Routes/auth.routes.e2e.spec.ts
@@ -1,14 +1,54 @@
 import { SETTINGS } from '@Bootstrap/Fastify/application.settings';
-import superagent from 'superagent';
+import superagent, { ResponseError } from 'superagent';
 
 const baseUrl = `${SETTINGS.protocol}://${SETTINGS.baseUrl}:${SETTINGS.port}`;
 
-describe('POST /login', () => {
+describe('Login', () => {
   it('should return 200', async () => {
     const res = await superagent
       .post(`${baseUrl}/auth/login`)
       .send({ identifier: 'admin@admin.fr', password: 'admin' });
     expect(res.status).toBe(200);
     expect(res.body.token).toEqual(expect.any(String));
+  });
+
+  it('should return 401', async () => {
+    try {
+      await superagent.post(`${baseUrl}/auth/login`).send({ identifier: 'error@admin.fr', password: 'admin' });
+    } catch (e) {
+      expect((e as ResponseError).status).toBe(401);
+    }
+  });
+});
+
+describe('SignUp', () => {
+  let adminAgent: ReturnType<typeof superagent.agent>;
+  beforeEach(async () => {
+    const res = await superagent
+      .post(`${baseUrl}/auth/login`)
+      .send({ identifier: 'admin@admin.fr', password: 'admin' });
+    adminAgent = superagent.agent().set('authorization', `Bearer ${res.body.token}`);
+  });
+
+  it('should return 200 with a failed operation', async () => {
+    const res = await superagent
+      .post(`${baseUrl}/auth/account/signup`)
+      .send({ identifier: 'admin@admin.fr', password: 'admin' });
+    expect(res.status).toBe(200);
+    expect(res.body.operationId).toEqual(expect.any(String));
+
+    const adminCheckRes = await adminAgent.get(`${baseUrl}/tracker/operations/${res.body.operationId}`);
+    expect(adminCheckRes.body.status).toBe('ERROR');
+  });
+
+  it('should return 200 with a successful operation', async () => {
+    const res = await superagent
+      .post(`${baseUrl}/auth/account/signup`)
+      .send({ identifier: 'user@user.fr', password: 'user' });
+    expect(res.status).toBe(200);
+    expect(res.body.operationId).toEqual(expect.any(String));
+
+    const adminCheckRes = await adminAgent.get(`${baseUrl}/tracker/operations/${res.body.operationId}`);
+    expect(adminCheckRes.body.status).toBe('SUCCESS');
   });
 });
